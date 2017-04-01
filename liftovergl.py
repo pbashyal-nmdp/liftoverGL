3d43b92637784499e42f4c206f26fbb190804c2f
Bob Milius <bmilius@nmdp.org>
2017-03-31 21:13:49 -0600

    first commit

---
+++ liftovergl.py
@@ -0,0 +1,250 @@
+#!/usr/bin/env python3
+"""
+pyliftgl.py
+
+copyright
+    Bob Milius, PhD
+    31 March 2017
+    National Marrow Donor Program/Be The Match
+
+Lifts over a GL String from one version of the IMGT/HLA database to another.
+
+This program uses the Allelelist_history.txt file from
+https://github.com/ANHIG/IMGTHLA
+
+Alleles are converted to HLA accession #s, and then mapped to directly alleles
+in the target version. If alleles do not exist in the target, then they are
+dropped from the target GL String. Alleles are not expanded to include new
+alleles. eg., the following alleles exist for 3.20.0 and 3.24.0:
+
+3.20.0
+HLA00053 HLA-A*24:03:01
+
+3.24.0
+HLA00053 HLA-A*24:03:01:01
+HLA14804 HLA-A*24:03:01:02
+
+When HLA-A*24:03:01 is converted from 3.20.0 to 3.25.0, it gets assigned to
+HLA-A*24:03:01:01
+and not
+HLA-A*24:03:01:01/HLA-A*24:03:01:02
+"""
+
+import argparse
+import pandas as pd
+import re
+import requests
+import sys
+
+# sample data
+G1 = ("HLA-A*02:06:01:01+HLA-A*11:01:01:01^"
+      "HLA-B*15:01:01:01+HLA-B*35:01:01:02^"
+      "HLA-C*03:02:02:01+HLA-C*04:01:01:01^"
+      "HLA-DPA1*02:02:02^"
+      "HLA-DPB1*05:01:01^"
+      "HLA-DQA1*03:01:01^"
+      "HLA-DQB1*03:02:01^"
+      "HLA-DRB1*04:03:01+HLA-DRB1*04:06:01^"
+      "HLA-DRB4*01:03:01:03")
+S1 = "3.22.0"
+T1 = "3.25.0"
+
+G2 = ("HLA-A*02:06:01:01+HLA-A*11:01:01:01/HLA-A*32:74^"
+      "HLA-B*15:01:01:01+HLA-A*32:74+HLA-B*35:01:01:02^"
+      "HLA-C*03:02:02:01+HLA-C*04:01:01:01~HLA-A*32:74^"
+      "HLA-DPA1*02:02:02^HLA-DPB1*05:01:01^HLA-A*32:74^"
+      "HLA-DQA1*03:01:01^HLA-DQB1*03:02:01^HLA-A*32:74+HLA-A*32:74^"
+      "HLA-DRB1*04:03:01/HLA-A*32:74/HLA-A*32:74/HLA-DRB1*04:06:01^"
+      "HLA-DRB4*01:03:01:03")
+S2 = "3.22.0"
+T2 = "3.0.0"
+
+G3 = ("HLA-A*01:01:01:01/HLA-A*01:02+HLA-A*24:03:01")
+S3 = "3.20.0"
+T3 = "3.25.0"
+
+
+def read_history():
+    """
+    reads the Allelelist_history.txt file that was downloaded from
+    https://github.com/ANHIG/IMGTHLA
+    into a pandas dataframe.
+    """
+    history = pd.read_csv("Allelelist_history.txt",
+                          sep="\t", header=0, index_col=0, dtype=str)
+    return history
+
+
+def mk_glids(glstring, version, history):
+    """
+    takes a GL String containing allele names for a version of
+    the IMGT/HLA database, and returns a GL String with HLA_IDs
+    substituted for the allele names
+    """
+    # the history file strips the '.' from the version, and pads the
+    # middle field to two spaces,
+    # so '3.1.0' becomes '3010'
+    v = version.split(".")
+    vers = ''.join([v[0], v[1].zfill(2), v[-1]])
+    for allele in get_alleles(glstring):
+        hla_id = history[history[vers] == allele[4:]].index.tolist()
+        if len(hla_id) == 1:
+            glstring = glstring.replace(allele, hla_id[0])
+        else:
+            print(allele, "has more than one id:", hla_id)
+            sys.exit()
+    # glstring = gl_clean(glstring)
+    return glstring
+
+
+def mk_target(gl_ids, target, history):
+    """
+    takes a GL String made up of HLA_IDs, and converts it to
+    one containing allele names for a specific IMGT/HLA version
+    """
+    # the history file strips the '.' from the version, and pads the
+    # middle field to two spaces,
+    # so '3.1.0' becomes '3010'
+    t = target.split(".")
+    targ = ''.join([t[0], t[1].zfill(2), t[-1]])
+    target_gl = gl_ids
+    for hla_id in get_alleles(gl_ids):
+        new_allele = "HLA-" + str(history[targ][hla_id])
+        target_gl = target_gl.replace(hla_id, new_allele)
+    target_gl = gl_clean(target_gl)
+    return target_gl
+
+
+def gl_clean(glstring):
+    """
+    takes a GL String that has just been lifted to another version,
+    and removes deleted alleles, and cleans up resulting
+    delimiters left behind
+    """
+    # remove non-existant alleles
+    glstring = glstring.replace("HLA-nan", "")
+    # after deleting the above, delimiters may be packed together. These have
+    # to removed when they are at the beginning or end of the GL String, or
+    # resolved to the highest precedence when they are in the middle of the
+    # GL String
+    # ---
+    # trailing delimiters
+    glstring = re.sub(r'[/~+|^]+$', '', glstring)
+    # leading delimiters
+    glstring = re.sub(r'^[/~+|^]+', '', glstring)
+    # the rest in order of precedence
+    glstring = re.sub(r'[/~+|^]*\^[/~+|^]*', '^', glstring)
+    glstring = re.sub(r'[/~+|]*\|[/~+|]*', '|', glstring)
+    glstring = re.sub(r'[/~+]*\+[/~+]*', '+', glstring)
+    glstring = re.sub(r'[/~]*\~[/+]*', '~', glstring)
+    glstring = re.sub(r'\/+', '/', glstring)
+    return glstring
+
+
+def get_alleles(glstring):
+    """
+    Takes a GL String, and returns a set containing all the alleles
+    """
+    alleles = set()
+    for allele in re.split(r'[/~+|^]', glstring):
+        alleles.add(allele)
+    return alleles
+
+
+def get_resource(glstring):
+    """
+    takes a GL String, and returns the resource type based on which
+    delimiters are present
+    """
+    if '^' in glstring:
+        return "multilocus-unphased-genotype"
+    elif '|' in glstring:
+        return "genotype-list"
+    elif '+' in glstring:
+        return "genotype"
+    elif '~' in glstring:
+        return "haplotype"
+    elif "/" in glstring:
+        return "allele-list"
+    else:
+        return "allele"
+
+
+def post_gl(glstring, version):
+    """
+    takes a GL String and version, and posts it to the gl.nmdp.org
+    and returns a dictionary containing the status code, the
+    response text, and the response location if successful
+    """
+    resource = get_resource(glstring)
+    url = 'http://gl.nmdp.org/imgt-hla/' + version + "/" + resource
+    headers = {'content-type': 'plain/text'}
+    response = requests.post(url, data=glstring, headers=headers)
+    if response.status_code == 201:
+        return {'status_code': response.status_code,
+                'text': response.text,
+                'location': response.headers['location']}
+    else:
+        return {'status_code': response.status_code,
+                'text': response.text}
+
+
+def get_gl(uri):
+    response = requests.get(uri)
+    return response
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-g", "--glstring",
+                       help="GL String to be converted",
+                       type=str)
+    group.add_argument("-u", "--uri",
+                       help="GL Service URI of GL String",
+                       type=str)
+    parser.add_argument("-s", "--source",
+                        required=True,
+                        help="Source IMGT/HLA version",
+                        type=str)
+    parser.add_argument("-t", "--target",
+                        help="Target IMGT/HLA version",
+                        default="3.25.0",
+                        type=str)
+    args = parser.parse_args()
+
+    args = parser.parse_args()
+    if (args.glstring is None) and (args.uri is None):
+        parser.error("at least one of --glstring or --uri required")
+
+    source = args.source
+    target = args.target
+
+    if args.uri:
+        response = get_gl(args.uri)
+        if response.status_code == 200:
+            gl = response.text
+        else:
+            print("status_code =", response.status_code)
+            print("text =", response.text)
+            sys.exit()
+    if args.glstring:
+        gl = args.glstring
+
+    # gl = G3
+    # source = S3
+    # target = T3
+    history = read_history()
+    # print(gl, "\n")
+    # s_uri = post_gl(gl, source)
+    # print(s_uri, "\n")
+    gl_ids = mk_glids(gl, source, history)
+    # print(gl_ids, "\n")
+    target_gl = mk_target(gl_ids, target, history)
+    # print(target_gl, "\n")
+    t_uri = post_gl(target_gl, target)
+    print(t_uri, "\n")
+
+
+if __name__ == "__main__":
+    main()
